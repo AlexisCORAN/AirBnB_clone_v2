454b6ffc9ddc0528587b810277eab113a3ac1384
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-27 04:48:54 +0000

    Added tasks

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This script sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+printf "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n<html>\n" |
+sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
