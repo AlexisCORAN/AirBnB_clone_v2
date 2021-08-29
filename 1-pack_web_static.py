57916f3bbc54113457d0314b4888d32af4254455
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-29 23:31:15 +0000

    Added tasks

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/urs/bin/python3
+"""
+THis file contains do_pack() function
+"""
+from fabric.api import local
+import os.path
+from datetime import datetime
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+
+    form = "%Y%m%d%H%M%S"
+    date = datetime.now().strftime(form)
+    file_name = "versions/web_static_{}.tgz".format(date)
+    if os.path.isdir("versions") is False:
+        local("mkdir versions")
+    local("tar -cvzf {} web_static".format(file_name))
+    return file_name
