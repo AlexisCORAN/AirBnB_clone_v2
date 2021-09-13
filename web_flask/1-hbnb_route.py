002f0c470282ff67241a808b3f2c2c765613dd84
AlexisCORAN <alexis_ca_@outlook.com>
2021-09-13 03:49:30 +0000

    Added tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def display_name():
+def display_hello():
     """ Prints 'Hello HBNB!' """
     return ('Hello HBNB!')
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ Prints 'HBNB' """
+    return ('HBNB')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
